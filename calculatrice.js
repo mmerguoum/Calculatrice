7d41187460c0922c87a2453d5274295d2e185137
mmerguoum <merguoummourad@gmail.com>
2021-12-01 10:38:41 +0100

    file css

--- calculatrice.js
+++ calculatrice.js
@@ -1,50 +1,74 @@
-// const numberButtons = document.querySelectorAll('.btn-num');
-// const operateButtons = document.querySelectorAll('.btn-op');
-// const clearButton = document.querySelector('#btn-clear');
-// const deleteButton = document.querySelector('#btn-delete');
-// const equalButton = document.querySelector('#btn-equal');
-// const dotButton = document.querySelector('#btn-dot');
-// const Screen = document.querySelector('#text');
+const numberButtons = document.querySelectorAll('.btn-num');
+const operateButtons = document.querySelectorAll('.btn-op');
+const clearButton = document.querySelector('#btn-clear');
+const deleteButton = document.querySelector('#btn-delete');
+const equalButton = document.querySelector('#btn-equal');
+const dotButton = document.querySelector('#btn-dot');
+const screen = document.querySelector('#text');
 // const topDisplay = document.querySelector('#screen-top');
-// const bottomDisplay = document.querySelector('#screen-bottom');
 
 
-// let operand1 = "";
-// let operand2 = "";
-// let operation = "";
+overwrite = true;
+let operand1 = "";
+let operand2 = "";
+let operation = "";
 
-// function add(a, b) {
-//     return a + b;
-// }
+function add(a, b) {
+    return a + b;
+}
 
-// function subtract(a, b) {
-//     return a - b;
-// }
+function subtract(a, b) {
+    return a - b;
+}
 
-// function multiply(a, b) {
-//     return a * b;
-// }
+function multiply(a, b) {
+    return a * b;
+}
 
-// function divide(a, b) {
-//     return a / b;
-// }
+function divide(a, b) {
+    return a / b;
+}
 
-// function operate(operator, a, b) {
-//     switch (operator) {
-//         case "+":
-//             return add(a, b);
-//         case "-":
-//             return subtract(a, b);
-//         case "x":
-//             return multiply(a, b);
-//         case "÷":
-//             if (b === 0) {
-//                 alert("Error: dividing with zero");
-//                 return;
-//             }
-//             return divide(a, b);
-//     }
-// }
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "x":
+            return multiply(a, b);
+        case "÷":
+            if (b === 0) {
+                alert("Error: dividing with zero");
+                return;
+            }
+            return divide(a, b);
+    }
+}
+function enterValue(e){
+if(screen.textCntent === 16 && overwrite === false){
+    return;
+}
+
+let value= e.target.textContent;
+console.log(value);
+
+if (screen.textContent === "0" && value === "0") {
+    return;
+}
+console.log(overwrite)
+if (overwrite === true) {
+    screen.textContent = value;
+    overwrite = false;
+} else {
+    screen.textContent += value;
+}
+}
+numberButtons.forEach(b => {
+    b.addEventListener('click', enterValue);
+});
+
+
 
 // var student = {
 //     fullName : "merguoum mourad",
