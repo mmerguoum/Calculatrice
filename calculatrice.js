97c6ba20e4c47e9cf492df4ff4445fdd57bf759f
mmerguoum <merguoummourad@gmail.com>
2021-12-01 09:28:13 +0100

    calculatrice

---
+++ calculatrice.js
@@ -0,0 +1,60 @@
+// const numberButtons = document.querySelectorAll('.btn-num');
+// const operateButtons = document.querySelectorAll('.btn-op');
+// const clearButton = document.querySelector('#btn-clear');
+// const deleteButton = document.querySelector('#btn-delete');
+// const equalButton = document.querySelector('#btn-equal');
+// const dotButton = document.querySelector('#btn-dot');
+// const Screen = document.querySelector('#text');
+// const topDisplay = document.querySelector('#screen-top');
+// const bottomDisplay = document.querySelector('#screen-bottom');
+
+
+// let operand1 = "";
+// let operand2 = "";
+// let operation = "";
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// function divide(a, b) {
+//     return a / b;
+// }
+
+// function operate(operator, a, b) {
+//     switch (operator) {
+//         case "+":
+//             return add(a, b);
+//         case "-":
+//             return subtract(a, b);
+//         case "x":
+//             return multiply(a, b);
+//         case "÷":
+//             if (b === 0) {
+//                 alert("Error: dividing with zero");
+//                 return;
+//             }
+//             return divide(a, b);
+//     }
+// }
+
+// var student = {
+//     fullName : "merguoum mourad",
+//     getFullName : function (){
+//      console.log(this);
+//      console.log(this.fullName);
+//     } 
+// }
+
+// var getFullName = student.getFullName;
+//var fullName = "class4";
+
+// student.getFullName();
